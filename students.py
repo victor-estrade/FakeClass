b3fa9a329e318564d4aad18e63feaf2431b0df09
Victor Estrade <victor.estrade@lri.fr>
2020-01-27 09:51:07 +0100

    update students list

--- students.py
+++ students.py
@@ -8,7 +8,7 @@ from git_call import git_remote_add_FakeClass
 
 STUDENTS = {
 'victor-estrade': 'Estrade Victor',
-# 'Didayolo':  'Pavao Adrien',
+'Didayolo':  'Pavao Adrien',
 'herilalaina': 'Rakotoarison Herilalaina',
 'fake_student':  'No Git Found test',
 }
